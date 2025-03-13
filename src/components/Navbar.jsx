495d05324cfc9b1eadaa7b9fbe14831bd5b7351c
Dani <alejandrorot2@gmail.com>
2025-03-13 15:56:29 +0100

    3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom";
+import '../styles/navbar.css';
 
-export const Navbar = () => {
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+const Navbar = () => {
+
+  return (
+    <div className="navbar-container">
+
+	<a href="">Characters</a>
+	<a href="">Veicles</a>
+	<a href="">Planets</a>
+	<a href="">Favorites</a>
+
+    <form className="navbar-search" role="search">
+    <input className="navbar-imput" type="search" placeholder="Search" aria-label="Search"/>
+    <button className="btn" type="submit"> Search </button>
+
+      </form>
+    </div>
+  );
 };
+
+export default Navbar;
