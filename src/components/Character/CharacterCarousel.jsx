f53fa0c2208003b6a1d7bbe6ba3550185cad4a6e
Dani <alejandrorot2@gmail.com>
2025-03-18 17:21:30 +0100

    1234

---
+++ src/components/Character/CharacterCarousel.jsx
@@ -0,0 +1,46 @@
+import './charactercarousel.css'
+import CharacterCard from './CharacterCard';
+
+const CharacterView = () => {
+
+    return (
+        <div className="carousel">
+            
+            <div className="list">
+                <div 
+                    className="item"
+                    style={{
+                        backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/Star_Wars_Logo.svg.png')"
+                    }} >
+                <div 
+                    className="item"
+                    style={{
+                        backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/Star_Wars_Logo.svg.png')"
+                    }} >
+                <div 
+                    className="item"
+                    style={{
+                        backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/Star_Wars_Logo.svg.png')"
+                    }} >
+                <div 
+                    className="item"
+                    style={{
+                        backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/Star_Wars_Logo.svg.png')"
+                    }} >
+                <div 
+                    className="item"
+                    style={{
+                        backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/Star_Wars_Logo.svg.png')"
+                    }} >
+               
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+    
+
+
+
+export default CharacterView;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,24 @@
 import '../styles/home.css';
-import CharacterView from '../components/Character/CharacterView';
+import CharacterView from '../components/Character/CharacterCarousel';
 
 
 
 export const Home = () => {
 
-
     return (
-
         <div className="carrousel">
 
-            <div className='list'>
-
-                <div className='item' style={(backgroung-image: url() )}
-
-                <CharacterView />
-
+            <div className="list">
+                <div 
+                    className="item"
+                    style={{
+                        backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/Star_Wars_Logo.svg.png')"
+                    }}
+                >
+                    <CharacterView />
+                </div>
             </div>
         </div>
-
-
     );
 };
 
