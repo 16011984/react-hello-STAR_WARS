19d7f27524c68183fce47f104dad238e02c74a9f
Dani <alejandrorot2@gmail.com>
2025-03-19 22:38:45 +0100

    carousel over

--- src/components/Character/CharacterCarousel.jsx
+++ src/components/Character/CharacterCarousel.jsx
@@ -11,6 +11,17 @@ const CharacterView = () => {
             <div className="list">
 
                 <div className="item" style={{ backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/luke-skywalker.jpg')" }} >
+                    <div className="content">
+
+                        <div className='name'> luke skywalker </div>
+                        <div className='des'> Lorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad     minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                        </div>
+
+                        <div className='btn'>
+                            <button> Add to Favorites </button>
+                            <button> Read More </button>
+                        </div>
+                    </div>
                 </div>
 
 
@@ -22,21 +33,31 @@ const CharacterView = () => {
 
                     <div className="content">
 
-                       
-                        <div className='name'> Name: </div>
-                        <div className='des'> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." </div>
-
-                        <div className='btn'>
-                        <button className= "btn1"> Favorites </button>
-                        <button className= "btn2"> See More </button>
+                        <div className='name'> Yoda </div>
+                        <div className='des'> Lorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad     minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
                         </div>
 
+                        <div className='btn'>
+                            <button> Add to Favorites </button>
+                            <button> Read More </button>
+                        </div>
                     </div>
 
 
                 </div>
 
                 <div className="item" style={{ backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/casian.jpeg')" }} >
+                    <div className="content">
+
+                        <div className='name'> Casian </div>
+                        <div className='des'> Lorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad     minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                        </div>
+
+                        <div className='btn'>
+                            <button> Add to Favorites </button>
+                            <button> Read More </button>
+                        </div>
+                    </div>
                 </div>
 
                 <div
@@ -44,6 +65,17 @@ const CharacterView = () => {
                     style={{
                         backgroundImage: "uhttps://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/R2D2.jpg')"
                     }} >
+                    <div className="content">
+
+                        <div className='name'> R2D2 </div>
+                        <div className='des'> Lorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad     minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                        </div>
+
+                        <div className='btn'>
+                            <button> Add to Favorites </button>
+                            <button> Read More </button>
+                        </div>
+                    </div>
                 </div>
 
                 <div
@@ -51,6 +83,17 @@ const CharacterView = () => {
                     style={{
                         backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/vader.jpg')"
                     }} >
+                    <div className="content">
+
+                        <div className='name'> Darth Vader </div>
+                        <div className='des'> Lorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua. Ut enim ad     minim veniam, quis nostrud  exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                        </div>
+
+                        <div className='btn'>
+                            <button> Add to Favorites </button>
+                            <button> Read More </button>
+                        </div>
+                    </div>
 
 
 
