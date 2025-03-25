03c03569640ec9dfaa4df9857b4e8b7392cbf67a
Dani <alejandrorot2@gmail.com>
2025-03-25 15:59:22 +0100

    cansado

--- src/components/Character/CharacterCarousel.jsx
+++ src/components/Character/CharacterCarousel.jsx
@@ -1,15 +1,27 @@
 import './charactercarousel.css'
 import React from 'react';
-import './Arrows';
+
 
 
 const CharacterCarousel = () => {
 
     return (
 
+
+
         <div className="carousel">
 
+
             <div className="list">
+
+                / ----NEXT----PREV----BUTTONS----//
+
+                <div className='arrows'>
+                    <bustton className='prev'> A </bustton>
+                    <button className='next'> AA </button>
+                    <div />
+                </div>
+
 
                 <div className="item" style={{ backgroundImage: "url('https://raw.githubusercontent.com/16011984/react-hello-STAR_WARS/refs/heads/master/src/assets/img/luke-skywalker.jpg')" }} >
                     <div className="content">
@@ -104,19 +116,13 @@ const CharacterCarousel = () => {
 
 
 
-                / ----NEXT----PREV----BUTTONS----//
 
-                <div className='arrows'>
-                    <button class="prev"> ← </button>
-                    <button class="next"> → </button>
-                    <div />
-                </div>
 
                 // ----TIME----RUNNING---- //
 
-                    <div className='timeRunning'></div>
+                <div className='timeRunning'></div>
 
-                
+
             </div>
         </div>
     )
